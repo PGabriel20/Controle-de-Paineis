baa20c433df18a48588c2257f050741cae8c704d
PGabriel20 <bielporreca@gmail.com>
2020-11-23 12:03:22 -0300

    Feat: add bcrypt

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 require('../models/Usuario')
 const Usuario = mongoose.model('usuarios')
+const bcrypt = require('bcryptjs')
 
 //Rota para registro
 router.get('/registro', (req,res) => {
@@ -10,12 +11,71 @@ router.get('/registro', (req,res) => {
 })
 
 router.post('/registro', (req,res) => {
+
     //Validação de formulario de registro
     var erros = []
 
     if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
-        erros.push({texto: 'Nome invalido'})
+        erros.push({texto: 'Nome invalido!'})
     }
+    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+        erros.push({texto: 'Email invalido!'})
+    }
+    if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
+        erros.push({texto: 'Senha invalida!'})
+    }
+    if(req.body.senha.length<4){
+        erros.push({texto: 'Senha muito pequena!'})
+    }
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto: 'Senhas não batem!'})
+    }
+    if(erros.length>0){
+        res.render('usuarios/registro', {erros: erros})
+    }
+    else{
+
+        //Verifica se email já existe
+        Usuario.findOne({email: req.body.email}).then((usuario)=>{
+            if(usuario){
+                req.flash('error_msg', 'Já existe uma conta com este email!')
+                res.redirect('/usuarios/registro')
+            }else{
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha,
+                })
+
+                bcrypt.genSalt(10, (erro, salt)=>{
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash)=>{
+                        if(erro){
+                            req.flash('error_msg', 'Houve um erro interno!')
+                            res.redirect('/')
+                        }
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(()=>{
+                            req.flash('success_msg', 'Usuario cadastrado com sucesso!')
+                            res.redirect('/')
+                        }).catch((err)=>{
+                            req.flash('error_msg', 'Houve um erro ao cadastrar usuario, tente novamente!')
+                            res.redirect('/usuarios/registro')
+                        })
+
+
+                    })
+                })
+
+            }
+        }).catch((err)=>{
+            req.flash('error_msg', 'Houve um erro interno!')
+            res.redirect('/')
+        })
+    }
+
 })
 
 
